87b3ab354162591978c4ea8ccd5252593f32fd2e
schmidtleon48 <leonjohannesschmidt@gmail.com>
2023-06-18 13:24:08 +0200

    kalman

--- kalman/state.py
+++ kalman/state.py
@@ -1,41 +1,107 @@
+'''
+Topic:          Kalman Filter
+Description:    Demonstration of the Kalman Filter
+Autor:          Leon Schmidt
+E-Mail:         leonjohannesschmidt@gmail.com
+'''
+
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.patches import Ellipse
+import kalman
 
 #define system
-iteration = 9
-A = np.array([[1.1, 0],[0,1.2]])
-H = np.array([1,2])
+iteration = 30
+var_mess = 2
+var_sys = 0.2
+var_state_init = 0.1
+
+A = np.array([[1.1, 0],[0,1.1]])
+B = np.identity(2)
+H = np.array([[1,0], [0,1]])
 inital = np.array([[-3], [-3]])
-noise_state = 0.2 * np.random.randn(2,9)
-noise_mess = 0.6 * np.random.randn(9)
+noise_state = var_sys * np.random.randn(2,iteration +2)
+noise_mess = var_mess * np.random.randn(iteration + 2)
+cov_nstate = np.array([[var_sys**2, 0],
+                      [0,var_sys**2]])
+cov_nmess = np.array([[var_mess**2,0],[0,2 * var_mess**2]])
+cov_state = np.array([[var_state_init,0],
+                     [0,var_state_init]])
 
-#propagation through system
+#propagation and visualization
 state_mom = inital 
+state_mom_est = inital
 i = 0
-state = inital
-output = []
-while i < iteration:
+fig, ax = plt.subplots(subplot_kw={'aspect': 'equal'})
+plt.pause(12)
+while i <= iteration:
+    #calculate ellipse
+    a = cov_state[0,0]
+    b = cov_state[0,1]
+    c = cov_state[1,1]
+
+    width = a + c + np.sqrt((a - c)**2  + 4 * b**2)
+    height = a + c - np.sqrt((a - c)**2  + 4 * b**2)
+    theta = 0 
+    if b == 0 and a >= c:
+        theta = 0
+    elif b == 0 and a < c:
+        theta = np.pi / 2
+    else:
+        theta = np.arctan2(width/ 2 -a, b)
+
+    angle = np.rad2deg(theta) 
 
 
+    #plot current state
+    ellipse = Ellipse((state_mom_est[0,0], state_mom_est[1,0]), width, height, angle=angle, alpha=0.3)
+    ax.add_artist(ellipse)
+    ax.set_title("State Space")
+    ax.set_xlabel("State 1")
+    ax.set_ylabel("State 2")
+    state_color = np.append(state_mom,state_mom_est, axis=1)
+    ax.scatter(state_color[0,:], state_color[1,:])
+    ax.set_xlim(-50, 0)
+    ax.set_ylim(-50, 0)
+    plt.draw()
+    plt.pause(0.2)
+
+    #propagate through system
+    state_mom_est = A @ state_mom
     state_mom = A @ state_mom + noise_state[:, i].reshape(2,1)
     output_mom = H @ state_mom
+    cov_state = A @ cov_state @ A.T + B @ cov_nstate @ B.T
 
-    state = np.concatenate((state, state_mom), axis = 1)
-    output.append(output_mom[0])
+    if i == 10 or i == 20:
+        K = kalman.kalman(cov_state, cov_nmess, H)
+        state_mom_est = state_mom_est + K @ (output_mom - H @ state_mom_est)
+        cov_state = cov_state - K @ H @ cov_state
+
+        #calculate ellipse
+        a = cov_state[0,0]
+        b = cov_state[0,1]
+        c = cov_state[1,1]
+
+        width = a + c + np.sqrt((a - c)**2  + 4 * b**2)
+        height = a + c - np.sqrt((a - c)**2  + 4 * b**2)
+        theta = 0 
+        if b == 0 and a >= c:
+            theta = 0
+        elif b == 0 and a < c:
+            theta = np.pi / 2
+        else:
+            theta = np.arctan2(width/ 2 -a, b)
+
+        angle = np.rad2deg(theta) 
+
+        ellipse = Ellipse((state_mom_est[0,0], state_mom_est[1,0]), width, height, angle=angle, alpha= 0.2, color = "red")
+        ax.add_artist(ellipse)
+        state_color = np.append(state_mom,state_mom_est, axis=1)
+        ax.scatter(state_mom_est[0,:], state_mom_est[1,:])
+        plt.draw()
+        plt.pause(1)
+
     i += 1
-
-
-steps = np.arange(9)
-for k in steps:
-    plt.clf() # Clear the current figure
-    
-    plt.scatter(state[0, k], state[1, k]) # Calculate and plot all you want
-    plt.xlim(-20,20)
-    plt.ylim(-50, 20)
-#    plt.fill_between(x, y, y+1, facecolor='C0', alpha=0.4)
-
-    plt.draw()
-    plt.pause(0.4) # Has to pause for a non zero time
 
 
 plt.show() # When all is done
